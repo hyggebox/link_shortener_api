2d695700ba89d8a4980ee360213a273152009df1
Anastasia <asya_apto@mail.ru>
2022-02-24 23:52:10 +0300

    Add paths

--- link_shortener/urls.py
+++ link_shortener/urls.py
@@ -7,5 +7,7 @@ from shortener import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_form),
-    path('<short_name>', views.redirect_to_full_url),
+    path('search', views.get_full_url),
+    path('add', views.convert_url),
+    path('<short_name>', views.redirect_to_full_url)
 ]
