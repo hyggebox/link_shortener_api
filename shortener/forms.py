ebad219da5fb77ee8aabf69207e4275e1f2b671e
Anastasia <asya_apto@mail.ru>
2022-02-26 01:26:09 +0300

    Fix full_url label

--- shortener/forms.py
+++ shortener/forms.py
@@ -3,9 +3,9 @@ from django.core.validators import validate_slug, URLValidator
 
 
 class ShortenLinkForm(forms.Form):
-    full_url = forms.CharField(label='Ссылка, которую нужно сократить',
-                                required=True,
-                                validators=[URLValidator()])
+    full_url = forms.CharField(label='Ссылка*',
+                               required=True,
+                               validators=[URLValidator()])
     custom_name = forms.CharField(label='Кастомное имя ссылки (по желанию)',
                                   validators=[validate_slug],
                                   required=False)
