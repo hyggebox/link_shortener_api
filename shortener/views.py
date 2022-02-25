e466c11e8df436ee2ab44961e93fe1c55d025d41
Anastasia <asya_apto@mail.ru>
2022-02-25 23:14:50 +0300

    Fix convert_url(), rename and add validators to create_short_url()

--- shortener/views.py
+++ shortener/views.py
@@ -1,7 +1,7 @@
 import string
 
 from django.contrib.sites.models import Site
-from django.core.validators import URLValidator
+from django.core.validators import URLValidator, validate_slug
 from django.core.exceptions import ValidationError
 from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
@@ -13,7 +13,7 @@ from .models import Link
 
 
 def convert_url(full_url, users_string):
-    if not users_string:
+    if users_string:
         short_name = users_string
     else:
         short_name = get_random_string(5, allowed_chars=string.ascii_lowercase)
@@ -41,11 +41,8 @@ def redirect_to_full_url(request, short_name):
     if users_link:
         return HttpResponseRedirect(users_link.full_url)
     return JsonResponse(
-        {
-            'Error': 'No such short url in the database'
-        },
-        status=404
-    )
+        {'Error': 'No such short url in the database'},
+        status=404)
 
 
 def get_full_url(request):
@@ -56,27 +53,30 @@ def get_full_url(request):
             current_site = Site.objects.get_current()
             return JsonResponse(
                 {
-                'full_url': users_link.full_url,
-                'short_url': f'http://{current_site.domain}/{short_name}'
+                    'full_url': users_link.full_url,
+                    'short_url': f'http://{current_site.domain}/{short_name}'
                 },
-                status=200
-            )
+                status=200)
         return JsonResponse(
-            {
-                'Error': 'No such short url in the database'
-            },
-            status=404
-        )
-    return HttpResponseRedirect('/')
+            {'Error': 'No such short url in the database'},
+            status=404)
+    return JsonResponse({'Error': 'No params provided'}, status=400)
 
-# @csrf_exempt
-def convert_url(request):
+
+@csrf_exempt
+def create_short_url(request):
     url_to_convert = request.POST.get('url', '')
     users_string = request.POST.get('short_name', '')
-    print(url_to_convert, users_string)
     validate_url = URLValidator()
     try:
         validate_url(url_to_convert)
+        try:
+            validate_slug(users_string)
+        except ValidationError:
+            return JsonResponse(
+                {'Error': 'short_name must consist of letters, numbers, '
+                          'underscores or hyphens.'},
+                status=400)
     except ValidationError:
         return JsonResponse({'Error': 'String is not valid URL'}, status=400)
     return JsonResponse(convert_url(url_to_convert, users_string), status=200)
