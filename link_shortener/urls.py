f1f66f844363b439d29965ba77f7313c5a2b8c09
Anastasia <asya_apto@mail.ru>
2022-02-25 23:18:10 +0300

    Rename convert_url() to create_short_url()

--- link_shortener/urls.py
+++ link_shortener/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_form),
     path('search', views.get_full_url),
-    path('add', views.convert_url),
+    path('add', views.create_short_url),
     path('<short_name>', views.redirect_to_full_url)
 ]
