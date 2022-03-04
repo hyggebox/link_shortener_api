e9183515cf2ea0fc9df42b8035feeb1a1f6430c7
Anastasia <asya_apto@mail.ru>
2022-03-04 18:01:14 +0300

    Change CharField to URLField

--- shortener/forms.py
+++ shortener/forms.py
@@ -1,11 +1,9 @@
 from django import forms
-from django.core.validators import validate_slug, URLValidator
+from django.core.validators import validate_slug
 
 
 class ShortenLinkForm(forms.Form):
-    full_url = forms.CharField(label='Ссылка*',
-                               required=True,
-                               validators=[URLValidator()])
+    full_url = forms.URLField(label='Ссылка*', required=True)
     custom_name = forms.CharField(label='Кастомное имя ссылки (по желанию)',
                                   validators=[validate_slug],
                                   required=False)
--- shortener/models.py
+++ shortener/models.py
@@ -5,8 +5,7 @@ class Link(models.Model):
     short_name = models.CharField(verbose_name='Короткое имя',
                                   max_length=100,
                                   primary_key=True)
-    full_url = models.CharField(verbose_name='Полная ссылка',
-                                max_length=200)
+    full_url = models.URLField(verbose_name='Полная ссылка', max_length=200)
 
     def __str__(self):
         return f'{self.short_name}'
